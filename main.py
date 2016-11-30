9ab42801e8b4e4bd1d49d38ba8111e28c7563670
Marc R. Preston <phyrenight@gmail.com>
2016-11-30 15:15:01 -0800

    refac

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 
 def main():
-   sentence = raw_input("Enter a sentence: ")
-   if ( len(sentence) == 0):
-       print"No string entered"
-   else:
-       convertToList(sentence)
-
+    lst = []
+    sentence = raw_input("Enter a sentence: ")
+    if ( len(sentence) == 0):
+        print"No string entered."
+    else:
+        lst = convertToList(sentence)
+        print(shift(lst))
+		
 def convertToList(sentence):
-    list = []
-    for items in sentence:
-        list.append(items)
+    if sentence == None or "":
+	    print "No string entered."
+    else:
+        lst = []
+        for items in sentence:
+            lst.append(items)
+        return	lst	
 
-    shift(list)
-
-def shift(list):
-    newList = []
-    for items in list:
-         if items.isalpha():
-             num = ord(items) + 1
-             newList.append(chr(num))	 
-         else:
-             newList.append(items)
-    putIntoAString(newList)
-	
+def shift(lst):
+    newSentence = ""
+    if lst.__len__() > 0:
+        for items in lst:
+            if items.isalpha():
+                num = ord(items)
+                if num == 121:
+                    newSentence += chr(97)
+                else:
+                    newSentence += (chr(num + 1))	 
+            else:
+                newSentence += items
+        return newSentence
+    else:
+        return "No String entered."
+'''	
 def putIntoAString(list):
     newSentence = ""
     for items in list:
         newSentence = newSentence + str(items)
     print newSentence	
+'''	
 			 
-        	
-	   
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
